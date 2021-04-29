7257ed29f6096527606175e2b95a541ad3f6daee
sabrina-ra <sbr.rabhi25@gmail.com>
2021-04-29 10:16:14 +0200

    brandController-base.html.twigu

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BaseController extends AbstractController
+class BrandController extends AbstractController
 {
-    #[Route('/base', name: 'base')]
+    #[Route('/brand', name: 'brand')]
     public function index(): Response
     {
-        return $this->render('base/index.html.twig', [
-            'controller_name' => 'BaseController',
+        return $this->render('brand/index.html.twig', [
+            'controller_name' => 'BrandController',
         ]);
     }
 }
